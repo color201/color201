fd4550e0339556c3be385ecd1ccb590355d322e4
color201 <color201@naver.com>
2020-05-11 15:10:16 +0900

    20200511_001

---
+++ js/common.js
@@ -0,0 +1,39 @@
+﻿jQuery(document).ready(function($) {
+	
+	"use strict";
+	
+	
+	// menu scroll fixed
+   $(window).scroll(function() {
+				
+      // main
+      if ($(document).scrollTop() > 300) {
+         $('.header-wrap').addClass('fixed scroll');
+							 // $('.basicTopSearch').addClass('displaynone');
+      } else {
+         $('.header-wrap').removeClass('fixed scroll');
+						  // $('.basicTopSearch').removeClass('displaynone');
+      }
+
+   });
+
+	
+									
+// tab icon on/off								
+$(function() { 
+    $(".off").hover(function(){ 
+       $(this).children("img").attr("src", $(this).children("img").attr("src").replace("off.png", "on.png")); 
+    }, function(){ 
+       $(this).children("img").attr("src", $(this).children("img").attr("src").replace("on.png", "off.png")); 
+    }); 
+	
+});
+	
+	
+	
+	
+	
+	
+	
+});
+																							
